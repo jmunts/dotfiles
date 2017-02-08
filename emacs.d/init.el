8c2ee7dbbfebf7dded579c957c688428a03ba720
John Mark Untalan <apeiron.jmunts@gmail.com>
2017-02-08 14:19:07 +0800

    add config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,8 +9,43 @@
 (cask-initialize)
 (require 'pallet)
 
-(load-theme 'material t)
-
 (projectile-global-mode)
 
 (windmove-default-keybindings)
+
+;; Use evil-mode
+(evil-mode 1)
+(global-evil-surround-mode 1)
+(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
+
+
+;; ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous)
+(flx-ido-mode 1) ; better/faster matching
+(setq ido-create-new-buffer 'always) ; don't confirm to create new buffers
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+;; exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; UI
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(when window-system
+  (scroll-bar-mode -1))
+
+(load-theme 'material t)
+
+;; Projectile
+(defun hrs/search-project-for-symbol-at-point ()
+  "Use `projectile-ag' to search the current project for `symbol-at-point'."
+  (interactive)
+  (projectile-ag (projectile-symbol-at-point)))
+
+(global-set-key (kbd "C-c v") 'projectile-ag)
+(global-set-key (kbd "C-c C-v") 'hrs/search-project-for-symbol-at-point)
