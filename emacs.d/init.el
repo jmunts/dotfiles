e8a810aba32a63b73810dd27c33cf475bdb3badb
John Mark Untalan <apeiron.jmunts@gmail.com>
2017-04-10 09:30:05 +0800

    added xah's copy current buffer file path

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -326,3 +326,26 @@
 ;; emacs profiler
 (use-package esup
   :ensure t)
+
+;; xah snippets
+(defun xah-copy-file-path (&optional *dir-path-only-p)
+  "Copy the current buffer's file path or dired path to `kill-ring'.
+Result is full path.
+If `universal-argument' is called first, copy only the dir path.
+URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
+Version 2017-01-27"
+  (interactive "P")
+  (let ((-fpath
+         (if (equal major-mode 'dired-mode)
+             (expand-file-name default-directory)
+           (if (buffer-file-name)
+               (buffer-file-name)
+             (user-error "Current buffer is not associated with a file.")))))
+    (kill-new
+     (if *dir-path-only-p
+         (progn
+           (message "Directory path copied: 「%s」" (file-name-directory -fpath))
+           (file-name-directory -fpath))
+       (progn
+         (message "File path copied: 「%s」" -fpath)
+         -fpath )))))
