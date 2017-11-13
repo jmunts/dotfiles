4a0244ac9f14b0836a0a077e0059a6683fc4b2a1
John Mark Untalan <jm@aelogica.com>
2017-11-13 10:44:32 +0800

    Add ediff config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -518,5 +518,18 @@ Version 2017-01-27"
 
 (global-undo-tree-mode)
 
+;; ediff
+(use-package ediff
+  :ensure t)
+
+;; don't start another frame
+;; this is done by default in preluse
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; put windows side by side
+(setq ediff-split-window-function (quote split-window-horizontally))
+;;revert windows on exit - needs winner mode
+(winner-mode)
+(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+
 (provide 'init)
 ;;; init.el ends here
