93f7b88d92cfcfe7b60f0f3c921e26016ad9572c
John Mark Untalan <apeiron.jmunts@gmail.com>
2018-01-24 20:02:23 +0800

    Add pkg-info and auto-package-update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -552,5 +552,17 @@ Version 2017-01-27"
   (setq dimmer-exclusion-regexp ".*helm.*"
         dimmer-percent 0.3))
 
+;; check package info
+(use-package pkg-info
+  :ensure t)
+
+;; auto update packages
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+         auto-package-update-interval 5)
+   (auto-package-update-maybe))
+
 (provide 'init)
 ;;; init.el ends here
